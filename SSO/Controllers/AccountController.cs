3029a8d74e19b9d2a28d134539eb110ef1deccf6
SwatiKaushal <73350136+SwatiKaushal@users.noreply.github.com>
2020-10-23 16:28:12 +0200

    Add files via upload

---
+++ SSO/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Authentication;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+//using System.Net.Http
+
+namespace SSO.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+            if (FormsAuthentication.Authenticate(username, password))
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login details");
+                ViewBag.ReturnUrl = returnUrl;
+                return View();
+            }
+        }
+    }
+}
